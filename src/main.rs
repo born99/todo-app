48355219b54319bdb88d0d176ba32d545318a998
Your Name <you@example.com>
2026-07-29 19:54:05 +0000

    feature: autostart

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,6 @@ fn main() {
         .plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent, Some(vec![])))
         .manage(db)
         .setup(move |app| {
-            // Forcefully enable OS auto-start on boot
             use tauri_plugin_autostart::ManagerExt;
             let _ = app.autolaunch().enable();
 
